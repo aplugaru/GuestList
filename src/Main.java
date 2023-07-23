2db08429924c4ce8627ca1ceb0f52dcdb6b5e357
andreea p <andreea_20av@yahoo.com>
2023-07-23 19:44:29 +0300

    adding and deleting guests

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -10,6 +11,7 @@ public class Main {
         Scanner scanner = new Scanner ( System.in );
 
         do {
+            System.out.println ();
             System.out.println ( "1 - Display All Guests" );
             System.out.println ( "2 - Add Guest" );
             System.out.println ( "3 - Remove Guest" );
@@ -23,7 +25,29 @@ public class Main {
                     System.out.println ( guests[i] );
                 }
             }
-                else if (option == 4) {
+
+            else if (option == 2) {
+                for (int i = 0; i < guests.length; i++) {
+                    if (guests [i] == null) {
+                        System.out.println ( "Name: " );
+                        guests[i] = scanner.next ();
+                        break;
+                    }
+                }
+            }
+
+            else if (option == 3) {
+                System.out.println ("Name: ");
+                String name = scanner.next ();
+                for (int i = 0; i<guests.length; i++) {
+                    if(guests[i] != null && guests[i].equals (name)) {
+                        guests[i] = null;
+                        break;
+                    }
+                }
+            }
+
+            else if (option == 4) {
                     System.out.println ("Exiting...");
                     break;
                 }
