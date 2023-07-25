cad829f19d06a8abdd5b826d2327078154c8bd17
andreea p <andreea_20av@yahoo.com>
2023-07-25 20:26:01 +0300

    exercises

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,18 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static String[] guests = new String[10];
+    static Scanner scanner = new Scanner ( System.in );
     public static void main(String[] args) {
 
-        String[] guests = new String[10];
 
-        Scanner scanner = new Scanner ( System.in );
 
         do {
-            System.out.println ();
-            System.out.println ( "1 - Display All Guests" );
-            System.out.println ( "2 - Add Guest" );
-            System.out.println ( "3 - Remove Guest" );
-            System.out.println ("4 - Exit");
-            System.out.println ( "Option: " );
-            int option = scanner.nextInt ();
-            System.out.println ();
+            displayGuests ();
+            displayMenu ();
+            int option = getOption ();
 
             if (option == 1) {
-                for (int i = 0; i < guests.length; i++) {
-                    System.out.println ( guests[i] );
-                }
-            }
-
-            else if (option == 2) {
                 for (int i = 0; i < guests.length; i++) {
                     if (guests [i] == null) {
                         System.out.println ( "Name: " );
@@ -36,7 +25,7 @@ public class Main {
                 }
             }
 
-            else if (option == 3) {
+            else if (option == 2) {
                 System.out.println ("Name: ");
                 String name = scanner.next ();
                 for (int i = 0; i<guests.length; i++) {
@@ -47,10 +36,31 @@ public class Main {
                 }
             }
 
-            else if (option == 4) {
+            else if (option == 3) {
                     System.out.println ("Exiting...");
                     break;
                 }
         } while (true) ;
+    }
+
+    static void displayGuests () {
+        System.out.println ("_________________________\n- GUESTS -\n");
+        for (int i = 0; i < guests.length; i++) {
+            System.out.println (guests[i] == null ? "--" : guests[i]);
+        }
+    }
+
+    static void displayMenu () {
+        System.out.println ("_________________________\n- MENU -\n");
+        System.out.println ( "1 - Add Guest" );
+        System.out.println ( "2 - Remove Guest" );
+        System.out.println ("3 - Exit");
+    }
+
+    static int getOption () {
+        System.out.println ( "Option: " );
+        int option = scanner.nextInt ();
+        System.out.println ();
+        return option;
     }
 }
